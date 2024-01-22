a618bde8dee2e82be46573a7d8ee2f3f4c395e30
Deepak Kumar <deepakchandrawanshi1063@gmail.com>
2024-01-23 02:19:21 +0530

    youtube videos carousel is working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { v4 as uuidv4 } from "uuid";
+import Card from "./Card";
+import Carousel from "./Carousel";
 
 function App() {
+  let cards = [
+    {
+      key: uuidv4(),
+      content: (
+        <Card imagen="https://www.youtube.com/embed/k8thEpyl7lc?si=Sy430KuTn2e5LXoU" />
+      ),
+    },
+    {
+      key: uuidv4(),
+      content: (
+        <Card imagen="https://www.youtube.com/embed/dilIYJkvd4Q?si=j3YfvEBaCfyjpHb1" />
+      ),
+    },
+    {
+      key: uuidv4(),
+      content: (
+        <Card imagen="https://www.youtube.com/embed/xgk0gKxNKmo?si=OojtNOrj1EH4nBWz" />
+      ),
+    },
+    {
+      key: uuidv4(),
+      content: (
+        <Card imagen="https://www.youtube.com/embed/8ny_ktspePw?si=kgQcDsu_VWUS7nRa" />
+      ),
+    },
+    {
+      key: uuidv4(),
+      content: (
+        <Card imagen="https://www.youtube.com/embed/17VvP36ijXw?si=_KcWR9XlQ8_pwK51" />
+      ),
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <Carousel
+        cards={cards}
+        height="500px"
+        width="30%"
+        margin="0 auto"
+        offset={2}
+        showArrows={false}
+      />
     </div>
   );
 }
